38a06c82ef76579fdef454f47d8dcae32e6ffe3b
Connor Twele <connortwele@gmail.com>
2020-08-24 07:22:48 -0400

    Updates

---
+++ precache-manifest.4f6638cc3b0b1ec3b7d74038de6a2169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5293b7b2bdeb01195be260c0ef12f2",
+    "url": "/exerciseTrackerProject/index.html"
+  },
+  {
+    "revision": "105aaca2c7faf10e5bb1",
+    "url": "/exerciseTrackerProject/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "105aaca2c7faf10e5bb1",
+    "url": "/exerciseTrackerProject/static/js/2.ce7d7127.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exerciseTrackerProject/static/js/2.ce7d7127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13fe6489fee4f7dbf65",
+    "url": "/exerciseTrackerProject/static/js/main.e8776e31.chunk.js"
+  },
+  {
+    "revision": "024b93a53a00c933a679",
+    "url": "/exerciseTrackerProject/static/js/runtime-main.6d6074c1.js"
+  }
+]);
